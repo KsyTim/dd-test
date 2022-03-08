8e0194a1f60fcbf23b2b96578586409a7c387bc2
KsyTim <timoshenko.ksenia.1998@gmail.com>
2022-03-08 16:21:42 +0400

    added arrow to top

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const videoBgc = document.querySelector('video').playbackRate = 3.0;
 const contentContainer = document.querySelector('.content-container');
 const onOffButton = document.querySelector('.content-onoff-btn');
+const result = document.querySelector('.header');
+const toTopArrow = document.querySelector('.arrow-top');
 const toggleClass = (elem, elemClass) => {
   document.querySelector(`${elem}`).classList.toggle(`${elemClass}`);
 }
@@ -9,17 +11,46 @@ const openCloseContent = (content) => {
   toggleClass('.content-onoff-arrow__up', 'show');
   toggleClass('.content-onoff-arrow__down', 'show');
 }
-
-document.addEventListener('click', (e) => {
-  const target = e.target;
-  if (target.matches('.content-onoff-btn') || target.matches('.content-onoff-arrow__down') || target.matches('.content-onoff-arrow__up')) {
-    toggleClass('.content-container', 'active');
-    let textContent = target.textContent;
-    console.log(textContent);
-    if (contentContainer.classList.length === 2) {
-      openCloseContent('Скрыть');
-    } else if (contentContainer.classList.length < 2) {
-      openCloseContent('Развернуть');
-    }
+const arrowVisibility = () => {
+  if (result.getBoundingClientRect().top < -100) {
+    toTopArrow.style.display = 'block';
+  } 
+  if (result.getBoundingClientRect().top >= -100) {
+    toTopArrow.style.display = 'none';
   }
-})
+}
+const arrowTop = () => {
+  arrowVisibility();
+  window.addEventListener('scroll', () => {
+    arrowVisibility();
+  })
+}
+const clickHandler = () => {
+  document.addEventListener('click', (e) => {
+    const target = e.target;
+    const targetToTop = target.closest('[href="#top"');
+    if (target.matches('.content-onoff-btn') || target.matches('.content-onoff-arrow__down') || target.matches('.content-onoff-arrow__up')) {
+      toggleClass('.content-container', 'active');
+      let textContent = target.textContent;
+      console.log(textContent);
+      if (contentContainer.classList.length === 2) {
+        openCloseContent('Скрыть');
+      } else if (contentContainer.classList.length < 2) {
+        openCloseContent('Развернуть');
+      }
+    }
+    if (targetToTop !== null) {
+      e.preventDefault();
+      const id = document.querySelector('[href="#top"').getAttribute('href').substring(1),
+        scrollTo = document.getElementById(id),
+        elemPosition = scrollTo.getBoundingClientRect().top;
+      window.scrollBy({
+        top: elemPosition,
+        behavior: 'smooth'
+      });
+    }
+  })
+}
+
+clickHandler();
+arrowTop();
